e56578afd5a79b566d6b04498191ce1098ad29bc
Pattest <baptistecadoux@gmail.com>
2022-07-04 10:52:45 +0200

    feat(Day 72): Project 14, part five
    - Locking our UI behind Face ID

--- BucketList/ViewModel/ContentView-ViewModel.swift
+++ BucketList/ViewModel/ContentView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -17,7 +18,8 @@ extension ContentView {
                                    longitudeDelta: 25))
         @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
-
+        @Published var isUnlocked: Bool = false
+ 
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
 
         init() {
@@ -55,6 +57,27 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
             }
         }
     }
